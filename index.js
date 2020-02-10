e1f566e163fd6d2a30463b8638d976462ddfde18
Rene <rdhb@pop-os.localdomain>
2020-02-10 14:34:37 -0600

    index.js listo para mostrar paginas y para enviar peticiones

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.static(__dirname + '/APP'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-
-var router = express.Router()
-router.use('/views',express.static('../APP'))
-app.use('/',router)
-
+//ESCUCHANDO A TRAVEZ DEL PUERTO 3000
 app.listen(port, () => {
     console.log(`aplicacion corriendo en http://localhost:${port}`)
-}) 
-
-app.get('/main', (req,res) => {
-    res.send({message: 'API'})
 })
 
+//CONFIGURANDO DIRECCIONES PARA LAS VISTAS
+app.get('/main', (req,res) => {
+    res.sendFile(__dirname + '/APP/main.html')
+})
+
+app.get('/login', (req,res) => {
+    res.sendFile(__dirname + '/APP/login.html')
+})
+
+
+//CONFIGURANDO LAS DIFERENTES PETICIONES GET, POST, PUT, DELETE
